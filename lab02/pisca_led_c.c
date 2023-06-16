2ae33eb99b858e177f44b28f881e188dc3cecac3
josemarquesaraujo <136395523+josemarquesaraujo@users.noreply.github.com>
2023-06-16 11:41:18 -0300

    Update pisca_led_c.c

--- lab02/pisca_led_c.c
+++ lab02/pisca_led_c.c
@@ -9,10 +9,13 @@
 
 #define GPIO_AMARELO "16"
 #define GPIO4_PATH_AMARELO "/sys/class/gpio/gpio16/"
+
 #define GPIO_VERMELHO "20"
 #define GPIO4_PATH_VERMELHO "/sys/class/gpio/gpio20/"
+
 #define GPIO_VERDE "21"
 #define GPIO4_PATH_VERDE "/sys/class/gpio/gpio21/"
+
 #define GPIO_SYSFS "/sys/class/gpio/"
 
 void writeGPIO(char filename[], char value[]){
