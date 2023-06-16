77474474ff0a863224c9ab7013a1fa109efaa5e8
josemarquesaraujo <136395523+josemarquesaraujo@users.noreply.github.com>
2023-06-16 11:10:09 -0300

    Update pisca_led.sh

--- lab02/pisca_led.sh
+++ lab02/pisca_led.sh
@@ -4,13 +4,11 @@
 # Derek Molloy, Exploring Raspberry Pi: Interfacing to the Real World with Embedded Linux,
 # Wiley 2016, ISBN 978-1-119-1868-1, http://www.exploringrpi.com/
 
-LED_GPIO=16  # Usar uma variavel facilita alteracoes futuras na porta usada
+GPIO_AMARELO=16  # Usar uma variavel facilita alteracoes futuras na porta usada
+GPIO_VERMELHO=20  # Usar uma variavel facilita alteracoes futuras na porta usada
+GPIO_VERDE=21  # Usar uma variavel facilita alteracoes futuras na porta usada
 
-# funcoes Bash
-function setLED
-{                                      # $1 eh o primeiro argumento passado para a funcao
-	echo $1 >> "/sys/class/gpio/gpio$LED_GPIO/value"
-}
+
 
 # Inicio do programa
 if [ $# -ne 1 ]; then                  # se nao houver exatamente um argumento passado ao programa
@@ -20,26 +18,12 @@ if [ $# -ne 1 ]; then                  # se nao houver exatamente um argumento p
 	exit 2                         # erro que indica numero invalido de argumentos
 fi
 
-echo "O comando passado foi: $1"
-
-if [ "$1" == "setup" ]; then
-	echo "Habilitando a GPIO numero $LED_GPIO"
+if [ "$1" == "ligar" ]; then
+	echo "Habilitando a GPIO numero $GPIO_AMARELO"
 	echo $LED_GPIO >> "/sys/class/gpio/export"
 	sleep 1                        # esperar 1 segundo para garantir que a gpio foi exportada
 	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO/direction"
-elif [ "$1" == "on" ]; then
-	echo "Acendendo o LED"
-	setLED 1                       # 1 eh recebido como $1 na funcao setLED
-elif [ "$1" == "off" ]; then
-	echo "Desligando o LED"
-	setLED 0                       # 0 eh recebido como $1 na funcao setLED
-elif [ "$1" == "status" ]; then
-	state=$(cat "/sys/class/gpio/gpio$LED_GPIO/value")
-	echo "O estado do LED eh: $state"
-elif [ "$1" == "close" ]; then
-	echo "Desabilitando a GPIO numero $LED_GPIO"
-	echo $LED_GPIO >> "/sys/class/gpio/unexport"
-else
-	echo "Comando nao reconhecido."
-	exit 3                         # erro que indica comando nao reconhecido
 fi
+
+	
+echo "Fim"
