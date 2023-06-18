5978636773c1bd96cd73f839ebd195663627e8df
josemarquesaraujo <136395523+josemarquesaraujo@users.noreply.github.com>
2023-06-18 19:20:19 -0300

    Update pisca_led.py

--- lab02/pisca_led.py
+++ lab02/pisca_led.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
 
-import sys
-from time import sleep
 
-SYSFS_DIR = "/sys/class/gpio/"
+import sys #necessário para obter acesso aos argumentos da linha de comando.
+from time import sleep #sleep insere atrasos no código, será usada para manter os leds acesos pelos tempos solicitados.
 
-LED_PATH_AMARELO = "/sys/class/gpio/gpio16/"
-LED_PATH_VERMELHO = "/sys/class/gpio/gpio20/"
-LED_PATH_VERDE = "/sys/class/gpio/gpio21/"
+SYSFS_DIR = "/sys/class/gpio/" #diretório onde estão armazenados os arquivos de controle da GPIO.
 
-LED_NUMBER_AMARELO = "16"
-LED_NUMBER_VERMELHO = "20"
-LED_NUMBER_VERDE = "21"
+LED_PATH_AMARELO = "/sys/class/gpio/gpio16/" #caminho para o diretório da GPIO 16
+LED_PATH_VERMELHO = "/sys/class/gpio/gpio20/" #caminho para o diretório da GPIO 20
+LED_PATH_VERDE = "/sys/class/gpio/gpio21/" #caminho para o diretório da GPIO 21
 
+LED_NUMBER_AMARELO = "16" #número do pino da GPIO
+LED_NUMBER_VERMELHO = "20" #número do pino da GPIO
+LED_NUMBER_VERDE = "21" #número do pino da GPIO
+
+#função responsável pela escrita em arquivos sysfs para controlar os leds.
 def writeLED(filename, value, path):
     fo = open(path + filename, "w")
     fo.write(value)
     fo.close()
+
+#o seguinte loop irá ativar os leds em ordem ao se escrever o número do pino GPIO no arquivo export, 
+#após isso será aguardado 0,1 segundo, o arquivo direction será configurado como saída, o valor do led passará a 1, 
+#acesso, será aguardado o tempo necessário de acordo com a cor do led, vermelho 2 segundos, verde e amarelo 1 segundo 
+#cada, após isso o valor do led passará a 0, apagado, e o pino GPIO será desativado por meio do comando unexport. O loop 
+#se repetirá 5 vezes e o programa será finalizado.
 
 for cont in range(5):
     writeLED(filename="export", value=LED_NUMBER_VERMELHO, path=SYSFS_DIR)
