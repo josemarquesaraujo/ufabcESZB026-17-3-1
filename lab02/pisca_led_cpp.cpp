159376ba9c1f0e75080a5d3a6cf876f655dc0499
josemarquesaraujo <136395523+josemarquesaraujo@users.noreply.github.com>
2023-06-18 20:19:34 -0300

    Update pisca_led_cpp.cpp

--- lab02/pisca_led_cpp.cpp
+++ lab02/pisca_led_cpp.cpp
@@ -3,55 +3,57 @@
 * Wiley 2016, ISBN 978-1-119-1868-1, http://www.exploringrpi.com/
 */
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <unistd.h>
+#include <iostream>/***/
+#include <fstream>/***/
+#include <string>/***/
+#include <unistd.h>/***/
 using namespace std;
 
-#define GPIO_AMARELO "16"
-#define GPIO_VERMELHO "20"
-#define GPIO_VERDE "21"
+#define GPIO_AMARELO "16"/***/
+#define GPIO_VERMELHO "20"/***/
+#define GPIO_VERDE "21"/***/
 
-#define GPIO_PATH_AMARELO "/sys/class/gpio/gpio16/"
-#define GPIO_PATH_VERMELHO "/sys/class/gpio/gpio20/"
-#define GPIO_PATH_VERDE "/sys/class/gpio/gpio21/"
+#define GPIO_PATH_AMARELO "/sys/class/gpio/gpio16/"/***/
+#define GPIO_PATH_VERMELHO "/sys/class/gpio/gpio20/"/***/
+#define GPIO_PATH_VERDE "/sys/class/gpio/gpio21/"/***/
 
-#define GPIO_SYSFS "/sys/class/gpio/"
+#define GPIO_SYSFS "/sys/class/gpio/"/***/
 
+/***/
 void writeGPIO(const string& path, const string& filename, const string& value) {
     ofstream fs((path + filename).c_str());
     fs << value;
     fs.close();
 }
-
+/***/
 int main(int argc, char* argv[]) {
     string cmd(argv[1]);
     int cont;
+    /***/
     for (cont = 0; cont <= 5; cont++) {
-        writeGPIO(GPIO_SYSFS, "export", GPIO_VERMELHO);
-        usleep(100000);
-        writeGPIO(GPIO_VERMELHO, "direction", "out");
-        writeGPIO(GPIO_VERMELHO, "value", "1");
-        usleep(2000000);
-        writeGPIO(GPIO_PATH_VERMELHO, "value", "0");
-        writeGPIO(GPIO_SYSFS, "unexport", GPIO_VERMELHO);
+        writeGPIO(GPIO_SYSFS, "export", GPIO_VERMELHO);/***/
+        usleep(100000);/***/
+        writeGPIO(GPIO_VERMELHO, "direction", "out");/***/
+        writeGPIO(GPIO_VERMELHO, "value", "1");/***/
+        usleep(2000000);/***/
+        writeGPIO(GPIO_PATH_VERMELHO, "value", "0");/***/
+        writeGPIO(GPIO_SYSFS, "unexport", GPIO_VERMELHO);/***/
 
-        writeGPIO(GPIO_SYSFS, "export", GPIO_VERDE);
-        usleep(100000);
-        writeGPIO(GPIO_VERDE, "direction", "out");
-        writeGPIO(GPIO_VERDE, "value", "1");
-        usleep(1000000);
-        writeGPIO(GPIO_PATH_VERDE, "value", "0");
-        writeGPIO(GPIO_SYSFS, "unexport", GPIO_VERDE);
+        writeGPIO(GPIO_SYSFS, "export", GPIO_VERDE);/***/
+        usleep(100000);/***/
+        writeGPIO(GPIO_VERDE, "direction", "out");/***/
+        writeGPIO(GPIO_VERDE, "value", "1");/***/
+        usleep(1000000);/***/
+        writeGPIO(GPIO_PATH_VERDE, "value", "0");/***/
+        writeGPIO(GPIO_SYSFS, "unexport", GPIO_VERDE);/***/
 
-        writeGPIO(GPIO_SYSFS, "export", GPIO_AMARELO);
-        usleep(100000);
-        writeGPIO(GPIO_AMARELO, "direction", "out");
-        writeGPIO(GPIO_AMARELO, "value", "1");
-        usleep(2000000);
-        writeGPIO(GPIO_PATH_AMARELO, "value", "0");
-        writeGPIO(GPIO_SYSFS, "unexport", GPIO_AMARELO);
+        writeGPIO(GPIO_SYSFS, "export", GPIO_AMARELO);/***/
+        usleep(100000);/***/
+        writeGPIO(GPIO_AMARELO, "direction", "out");/***/
+        writeGPIO(GPIO_AMARELO, "value", "1");/***/
+        usleep(2000000);/***/
+        writeGPIO(GPIO_PATH_AMARELO, "value", "0");/***/
+        writeGPIO(GPIO_SYSFS, "unexport", GPIO_AMARELO);/***/
     }
 
     return 0;
