0a7fc1772a359d1d66ce78896564c1a981e6c5a8
josemarquesaraujo <136395523+josemarquesaraujo@users.noreply.github.com>
2023-06-28 19:49:46 -0300

    Update LED.ino

--- lab04/LED.ino
+++ lab04/LED.ino
@@ -1,1 +1,12 @@
+const int analogInPin = A0;                  // o potenciômetro esta ligado ao pino A0
 
+void setup(){
+   // Configura a serial: baud rate de 115200, 8-bit, sem paridade, 1 stop bit
+   Serial.begin(115200, SERIAL_8N1);
+}
+
+void loop(){
+   int valor = analogRead(analogInPin);      // le valor no pino A0 usando conversor ADC de 10-bit
+   Serial.println(valor);             // envia os valores pela porta serial
+   delay(1000);
+}
